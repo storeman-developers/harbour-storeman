a9f6dcb507a59019b444660cdb467222c494eb36
Petr Tsymbarovich <petr@tsymbarovich.ru>
2018-05-23 23:39:01 +0300

    Use old libsailfishapp API

--- harbour-storeman/src/harbour-storeman.cpp
+++ harbour-storeman/src/harbour-storeman.cpp
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
     NetworkAccessManagerFactory factory;
     view->engine()->setNetworkAccessManagerFactory(&factory);
 
-    view->setSource(SailfishApp::pathToMainQml());
+    view->setSource(SailfishApp::pathTo(QStringLiteral("qml/harbour-storeman.qml")));
     view->show();
 
     return app->exec();
