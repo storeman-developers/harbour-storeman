dbdbc7523670730e493541c3d9f5f2b85e01f359
Petr Tsymbarovich <petr@tsymbarovich.ru>
2020-09-23 15:23:12 +0300

    Update update_categories.py

--- scripts/update_categories.py
+++ scripts/update_categories.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 from urllib import request
 import json
+import re
 
-URL = 'https://openrepos.net/api/v1/categories'
-TEMPL = '''\
-//% "{}"
-{{ {}, QT_TRID_NOOP("orn-cat-{}") }}'''
-
+URL     = 'https://openrepos.net/api/v1/categories'
+PATTERN = re.compile(r'[ &]+')
 
 def process(json_array):
-    result = []
     for o in json_array:
-        tid = o['tid'].rjust(4)
+        tid  = o['tid'].rjust(4)
         name = o['name']
-        trid = name.replace(' & ', '-').replace(' ', '-').lower()
-        result.append(TEMPL.format(name, tid, trid))
+        trid = PATTERN.sub('-', name).lower()
+        print(f'//% "{name}"\n{{ {tid}, QT_TRID_NOOP("orn-cat-{trid}") }},')
         if 'childrens' in o:
-            result.extend(process(o['childrens']))
-    return result
-
-def main():
-    response = request.urlopen(URL)
-    cats = json.loads(response.read().decode('UTF-8'))
-    result = process(cats)
-    print(',\n'.join(result))
+            process(o['childrens'])
 
 if __name__ == '__main__':
-    main()
+    response   = request.urlopen(URL)
+    categories = json.loads(response.read().decode('UTF-8'))
+    process(categories)
