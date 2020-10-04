387ebf8343595171ffbb144f6cc6bf454ad19300
Petr Tsymbarovich <petr@tsymbarovich.ru>
2020-10-04 18:15:47 +0300

    Update translations

--- translations/harbour-storeman-es.ts
+++ translations/harbour-storeman-es.ts
@@ -1053,11 +1053,11 @@
     </message>
     <message id="orn-donate-with">
         <source>Donate with %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Donar con %1</translation>
     </message>
     <message id="orn-cover-updates-available">
         <source>Updates available</source>
-        <translation type="unfinished">Actualizaciones disponibles</translation>
+        <translation>Actualizaciones disponibles</translation>
     </message>
 </context>
 </TS>
