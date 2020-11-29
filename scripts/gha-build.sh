b0a52e5c46006388dc9033f63cf87699fae746c2
Petr Tsymbarovich <petr@tsymbarovich.ru>
2020-11-29 11:33:21 +0300

    Update build script
    Perform a shadow build

--- scripts/gha-build.sh
+++ scripts/gha-build.sh
@@ -3,8 +3,11 @@
 docker run --rm --privileged -v $PWD:/share coderus/sailfishos-platform-sdk:$RELEASE /bin/bash -c "
   set -x
   export LATEST_RELEASE=$LATEST_RELEASE
-  mkdir -p build;
-  cd build;
-  cp -r /share/* .;
-  mb2 -t SailfishOS-$RELEASE-$ARCH build -d;
+  mkdir -p tree
+  cd tree
+  cp -r /share/* .
+  cd ..
+  mkdir -p build
+  cd build
+  mb2 -t SailfishOS-$RELEASE-$ARCH build -d ../tree
   sudo cp -r RPMS/*.rpm /share/RPMS"
