99a78ae388856504fcda5c6b35568e902889102e
olf <Olf0@users.noreply.github.com>
2022-07-16 21:22:48 +0200

    [TS source file] id="orn-donate-with" → "orn-donate"

--- translations/harbour-storeman.ts
+++ translations/harbour-storeman.ts
@@ -1011,9 +1011,9 @@
         <source>My repository</source>
         <translation>My repository</translation>
     </message>
-    <message id="orn-donate-with">
-        <source>Donate with %1</source>
-        <translation>Donate with %1</translation>
+    <message id="orn-donations">
+        <source>Donations</source>
+        <translation>Donations</translation>
     </message>
     <message id="orn-cover-updates-available">
         <source>Updates available</source>
