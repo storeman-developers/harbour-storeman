637dffc65d4f8b5fda1a37736383453962bdf5f0
olf <Olf0@users.noreply.github.com>
2022-07-17 18:50:27 +0200

    Update harbour-storeman-tt.ts

--- translations/harbour-storeman-tt.ts
+++ translations/harbour-storeman-tt.ts
@@ -1009,9 +1009,9 @@ Storeman серсүзне өченче затларга җибәрми.</transla
         <translation>%0 пакеты уңышлы яңартылды</translation>
     </message>
     <message id="orn-app-description-full">
-        <source>&lt;p&gt;OpenRepos client application for SailfishOS&lt;/p&gt;&lt;p&gt;Storeman is Free Software (FLOSS), distributed under the terms of the &lt;a href=&apos;%1&apos;&gt;MIT&amp;nbsp;license&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Issues shall be reported preferably at GitHub or alternatively at OpenRepos (use the buttons below).&lt;/p&gt;</source>
+        <source>&lt;p&gt;OpenRepos client application for SailfishOS&lt;br /&gt;&amp;nbsp;&lt;/p&gt;&lt;p&gt;Storeman is Free Software (FLOSS), distributed under the terms of the &lt;a href=&apos;%1&apos;&gt;MIT&amp;nbsp;license&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Issues shall be reported preferably at GitHub or alternatively at OpenRepos (use the buttons below).&lt;/p&gt;</source>
         <extracomment>Avoid to translate the acronym FLOSS (Free, Libre, Open Source Software).</extracomment>
-        <translation>&lt;p&gt;SailfishOS өчен бердәм OpenRepos клиенты&lt;/p&gt;&lt;p&gt;Storeman&apos;ның чыганак коды &lt;a href=&apos;%1&apos;&gt;MIT&amp;nbsp;лицензиясе&lt;/a&gt; шартлары буенча тарала.&lt;/p&gt;&lt;p&gt;Тәкъдимнәрне GitHub һәм OpenRepos сайтлары аша җиткереп була (астагы төймәләр).&lt;/p&gt;</translation>
+        <translation>&lt;p&gt;SailfishOS өчен бердәм OpenRepos клиенты&lt;br /&gt;&amp;nbsp;&lt;/p&gt;&lt;p&gt;Storeman&apos;ның чыганак коды &lt;a href=&apos;%1&apos;&gt;MIT&amp;nbsp;лицензиясе&lt;/a&gt; шартлары буенча тарала.&lt;/p&gt;&lt;p&gt;Тәкъдимнәрне GitHub һәм OpenRepos сайтлары аша җиткереп була (астагы төймәләр).&lt;/p&gt;</translation>
     </message>
     <message id="orn-show-details">
         <source>Show details</source>
