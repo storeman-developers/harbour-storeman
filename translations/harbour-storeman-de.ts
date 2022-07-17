26ea953c2a666370683069870918fb598bbad161
olf <Olf0@users.noreply.github.com>
2022-07-17 18:57:48 +0200

    Update harbour-storeman-de.ts

--- translations/harbour-storeman-de.ts
+++ translations/harbour-storeman-de.ts
@@ -1008,6 +1008,7 @@
     </message>
     <message id="orn-app-description-full">
         <source>&lt;p&gt;OpenRepos client application for SailfishOS&lt;br /&gt;&amp;nbsp;&lt;/p&gt;&lt;p&gt;Storeman is Free Software (FLOSS), distributed under the terms of the &lt;a href=&apos;%1&apos;&gt;MIT&amp;nbsp;license&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Issues shall be reported preferably at GitHub or alternatively at OpenRepos (use the buttons below).&lt;/p&gt;</source>
+        <extracomment>Avoid to translate the acronym FLOSS (Free, Libre, Open Source Software).</extracomment>
         <translation>&lt;p&gt;OpenRepos-Client App für SailfishOS&lt;br /&gt;&amp;nbsp;&lt;/p&gt;&lt;p&gt;Storeman ist Freie Software (FLOSS), die unter den Bedingungen der &lt;a href=&apos;%1&apos;&gt;MIT-Lizenz&lt;/a&gt; verbreitet wird.&lt;/p&gt;&lt;p&gt;Fehler sollen vorzugsweise auf GitHub oder alternativ auf OpenRepos gemeldet werden (verwende dafür die untenstehenden Schaltflächen).&lt;/p&gt;</translation>
     </message>
     <message id="orn-show-details">
