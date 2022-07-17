7f40f9b912e16146c1bc0d7e66cade5c11f99fe0
olf <Olf0@users.noreply.github.com>
2022-07-17 04:43:14 +0200

    Update harbour-storeman-nl.ts

--- translations/harbour-storeman-nl.ts
+++ translations/harbour-storeman-nl.ts
@@ -1011,8 +1011,8 @@
         <source>My repository</source>
         <translation>Mijn pakketbron</translation>
     </message>
-    <message id="orn-donations">
-        <source>Donations</source>
+    <message id="orn-donation">
+        <source>Donation</source>
         <translation>Doneren</translation>
     </message>
     <message id="orn-cover-updates-available">
@@ -1024,8 +1024,9 @@
         <translation>Pakket %0 is bijgewerkt</translation>
     </message>
     <message id="orn-app-description-full">
-        <source>&lt;p&gt;A native OpenRepos.net client app for SailfishOS&lt;/p&gt;&lt;p&gt;Storeman is Free Software (FLOSS), distributed under the terms of the &lt;a href=&apos;%1&apos;&gt;MIT&amp;nbsp;License&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Issues can be reported preferably at GitHub or alternatively at OpenRepos.net (e.g., by using the buttons below).&lt;/p&gt;</source>
-        <translation>&lt;p&gt;OpenRepos.net-cliënt voor SailfishOS&lt;/p&gt;&lt;p&gt;Dit is vrije software, uitgegeven onder de voorwaarden van de &lt;a href=&apos;%1&apos;&gt;MIT-licentie&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Je kunt een probleem melden op OpenRepos.net of op GitHub (gebruik daarvoor de knoppen hieronder).&lt;/p&gt;</translation>
+        <source>&lt;p&gt;OpenRepos client application for SailfishOS&lt;/p&gt;&lt;p&gt;Storeman is Free Software (FLOSS), distributed under the terms of the &lt;a href=&apos;%1&apos;&gt;MIT&amp;nbsp;license&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Issues shall be reported preferably at GitHub or alternatively at OpenRepos (use the buttons below).&lt;/p&gt;</source>
+        <extracomment>Avoid to translate the acronym FLOSS (Free, Libre, Open Source Software).</extracomment>
+        <translation>&lt;p&gt;OpenRepos-cliënt voor SailfishOS&lt;/p&gt;&lt;p&gt;Dit is vrije software (FLOSS), uitgegeven onder de voorwaarden van de &lt;a href=&apos;%1&apos;&gt;MIT-licentie&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Je kunt een probleem melden op GitHub of op OpenRepos (gebruik daarvoor de knoppen hieronder).&lt;/p&gt;</translation>
     </message>
     <message id="orn-show-details">
         <source>Show details</source>
