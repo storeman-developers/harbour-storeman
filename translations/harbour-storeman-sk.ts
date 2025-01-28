53790abf75af7531a06d66999d256f8b241bc6d9
olf <Olf0@users.noreply.github.com>
2025-01-28 03:53:51 +0100

    [harbour-storeman-sk.ts] Update `<extracomment>` for `orn-username`

--- translations/harbour-storeman-sk.ts
+++ translations/harbour-storeman-sk.ts
@@ -398,7 +398,7 @@
     </message>
     <message id="orn-username">
         <source>Username</source>
-        <extracomment>A translated string should not be longer than the original</extracomment>
+        <extracomment>A translated string should comprise less than 27 characters</extracomment>
         <translation>Meno používateľa</translation>
     </message>
     <message id="orn-login-help">
