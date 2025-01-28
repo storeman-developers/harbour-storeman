6af978585eb4d4d06a52a17788a461f6f1718dbb
olf <Olf0@users.noreply.github.com>
2025-01-28 03:37:26 +0100

    [harbour-storeman-zh.ts] E-mail address does not seem to work …
    …, though it should: OpenRepos' web-frontend allows for both, either an OpenRepos user-name or its e-mail address, and AFAIK the OpenRepos API documentation states that; still only the user-name seems to work when using the web-API.

--- translations/harbour-storeman-zh.ts
+++ translations/harbour-storeman-zh.ts
@@ -402,9 +402,9 @@
         <translation>登录</translation>
     </message>
     <message id="orn-username">
-        <source>Username or e-mail address</source>
-        <extracomment>A translated string should not be longer than the original</extracomment>
-        <translation>用户名称或邮箱地址</translation>
+        <source>Username</source>
+        <extracomment>A translated string should comprise less than 27 characters</extracomment>
+        <translation>用户名</translation>
     </message>
     <message id="orn-login-help">
         <source>Log in to OpenRepos.net to comment applications and reply to others comments.&lt;br /&gt;&lt;br /&gt;Storeman does not send your password to third-parties.</source>
